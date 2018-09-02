feac3b69bbff1daffd34765f0e68fc071b5bb248
Dan Hintz <dhintz@live.com>
2018-09-02 19:57:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,3 +5,15 @@ class Dog
     @name = name
     @@dogs << self
   end
+  
+  def self.all
+    puts @@all.collect {|dog| dog.name}
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+end
+  
+  
